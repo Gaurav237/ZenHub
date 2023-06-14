0eb34f70cdb84460484f85907e0e29ead9161fde
Gaurav237 <gktiwary237@gmail.com>
2023-06-15 02:43:39 +0530

    setup ejs view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 // routes => for handling requests made to url('/')
 app.use('/', require('./routes'));
 
+// setting up template view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server on port : ${err}`);
