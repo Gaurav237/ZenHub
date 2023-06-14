2d367316097940137fab1f679b26c6231331588d
Gaurav237 <gktiwary237@gmail.com>
2023-06-15 03:00:13 +0530

    added user-sign-in/-up router|controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,3 +2,14 @@ module.exports.profile = function(req, res){
     return res.end('<h1> Profile Page </h1>');
 }
 
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'ZenBook | Sign Up'
+    });
+};
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'ZenBook | Sign In'
+    });
+};
