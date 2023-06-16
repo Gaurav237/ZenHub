ff611cc38bca4cba7e24d6bdf09e838d00502a12
Gaurav237 <gktiwary237@gmail.com>
2023-06-16 08:47:38 +0530

    used express-session-cookies for passport-authentication

--- index.js
+++ index.js
@@ -2,19 +2,36 @@ const express = require('express');
 const port = 8000;
 const cookieParser = require('cookie-parser');
 const app = express();
-
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-startegy');
 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 
-// routes => for handling requests made to url('/')
-app.use('/', require('./routes'));
-
 // setting up template view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// middleware used for encrypting session cookies
+app.use(session({
+    name: 'zenhub',
+    secret: 'zenhub', // key used for encryption
+    saveUninitialized: false, // will not save uninitialized(no modifications) sessions to the session store
+    resave: false, // will only save the session object to the session store if it has been modified during the request.
+    cookie: {
+        maxAge: (1000 * 60 * 100)  // expiry age(ms) of the session cookie
+    }
+}));
+
+app.use(passport.initialize());  // initializes Passport.js.
+app.use(passport.session());  // enables session support for Passport.js
+
+// routes => for handling requests made to url('/')
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
+const passport = require('passport');
 
 router.get('/profile', usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 router.post('/create', usersController.create);
+router.post('/create-session', passport.authenticate(
+    'local',  // name of the authentication strategy 
+    {failureRedirect: '/users/sign-in'}  // if the authentication fails.
+), usersController.createSession);
 
 module.exports = router;
