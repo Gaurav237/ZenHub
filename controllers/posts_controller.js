86e0c729296a9ded376ec8a7278eb66595f7a58d
Gaurav237 <gktiwary237@gmail.com>
2023-06-16 23:55:56 +0530

    delete post route

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res) {
     try {
@@ -13,3 +14,25 @@ module.exports.create = async function(req, res) {
         return res.redirect('back');
     }
 };
+
+// delete post & all its comments
+module.exports.destroy = async function(req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // * .id is string converted from ObjectId(._id)
+        // * post.user by default contains user id in string format only (if not populated)
+        if(post.user == req.user.id){   
+            post.deleteOne();
+            
+            // delete all comments of that post
+            let comments = await Comment.deleteMany({post: req.params.id});
+        
+            return res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
+    }catch(err){
+        return res.redirect('back');
+    }
+}
