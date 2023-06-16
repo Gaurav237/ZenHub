55097a64ce7b41d311ad05a29c6ec94ba2ec8bc4
Gaurav237 <gktiwary237@gmail.com>
2023-06-16 22:47:35 +0530

    displaying posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,17 @@
-module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+const Post = require('../models/post');
+
+module.exports.home = async function(req, res){
+    try {
+        let posts = await Post.find({}) 
+            .sort('-createdAt')
+            .populate('user');
+        
+        return res.render('home', { 
+            title: 'Home', 
+            posts: posts, 
+        }); 
+    }catch(err){
+        console.log('error in fetching posts from db');
+    }
 }
 
