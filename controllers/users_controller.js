44f1813dba94bb1756779961d255801db1a5f70b
Gaurav237 <gktiwary237@gmail.com>
2023-06-16 09:03:48 +0530

    restricting page access

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,12 +5,23 @@ module.exports.profile = function(req, res){
 }
 
 module.exports.signUp = function(req, res){
+    // restricting page access
+    // once signed up, then we cant again go to sign-up OR sign-in page using url 
+    // isAuthenticated() => used to check if a user is authenticated or logged in.
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: 'ZenBook | Sign Up'
     });
 };
 
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: 'ZenBook | Sign In'
     });
