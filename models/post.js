c690b973dce5b7ea2e4cdfa246b1d006af6d5837
Gaurav237 <gktiwary237@gmail.com>
2023-06-16 22:53:46 +0530

    added Comment Schema && included Comments Id as array in Post Schema

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,  // user object id is always unique
         ref: 'User'  // reference to User Schema
-    }
+    },
+    // inlcude all the ids of comments in this post schema in an array
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 }, {
     timestamps: true
 });
